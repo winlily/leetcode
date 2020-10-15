416b438bf45b426a8cd45a1ec16486f7d573a5a7
winlily <winlily@winlilydeMacBook-Pro.local>
2020-10-15 18:06:57 +0800

    :tada: adding strStr

--- daily/2020-10-15.js
+++ daily/2020-10-15.js
@@ -1,5 +1,6 @@
 /**
  * 题目：判断一个整数是否是回文数。回文数是指正序（从左向右）和倒序（从右向左）读都是一样的整数。
+ * 难度：简单
  * 思路：先转为字符串，再使用双指针。
  */
 function checkIsPalindrome(num) {
@@ -15,4 +16,21 @@ function checkIsPalindrome(num) {
     }
     return true
 }
-var res = checkIsPalindrome(-11211)
+
+/**
+ * 题目：找出子字符串首次出现的位置
+ * 难度：简单
+ * 思路：循环遍历，字符串截取并与目标子字符串比较。
+ */
+function strStr(str, subStr) {
+    if (subStr.length === 0) {
+        return 0
+    }
+    for (var i = 0; i < str.length; i++) {
+        var newStr = str.substr(i, subStr.length)
+        if (newStr === subStr) {
+            return i
+        }
+    }
+    return -1
+}
